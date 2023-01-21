813f861093d67394acf2f9ff98da30a75e6534b8
kenbadertscher <badertscher.ken@gmail.com>
2023-01-21 17:30:40 -0500

    SEND IT

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './whatgpt3.css';
+
+const WhatGPT3 = () => {
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      
+    </div>
+  )
+}
+
+export default WhatGPT3;
